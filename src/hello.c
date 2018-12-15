2c099528666b2849ecb4c998351e0c88359bb81a
Orçun Özdemir <benorcunozdemir@gmail.com>
2018-12-15 11:24:01 +0300

    Add and fix docs and comments

--- src/hello.c
+++ src/hello.c
@@ -14,25 +14,25 @@
 #include <errno.h>
 #include <fcntl.h>
 
-static const char *hello_str = "Hello Wsdsorld!\n";
+static const char *hello_str = "Hello World!\n";
 static const char *hello_path = "/hello";
 
-static int hello_getattr(const char *path, struct stat *stbuf)
+static int hello_getattr(const char *path, struct stat *stbuf)  // Stat struct keeps information about a file.
 {
     int res = 0;
 
     memset(stbuf, 0, sizeof(struct stat));
     if (strcmp(path, "/") == 0) {
-        stbuf->st_mode = S_IFDIR | 0755;
-        stbuf->st_nlink = 2;
+        stbuf->st_mode = S_IFDIR | 0755;    // If directory?
+        stbuf->st_nlink = 2;                // Number of hard links to the file.
     }
     else if (strcmp(path, hello_path) == 0) {
-        stbuf->st_mode = S_IFREG | 0444;
+        stbuf->st_mode = S_IFREG | 0444;    // If regular file?
         stbuf->st_nlink = 1;
         stbuf->st_size = strlen(hello_str);
     }
     else
-        res = -ENOENT;
+        res = -ENOENT; // No such file or directory. 
 
     return res;
 }
